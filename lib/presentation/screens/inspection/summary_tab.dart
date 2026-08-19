34eeea930a4134c95c4ee9d1cef8ce3549de8524
unknown <f4r3ver@gmail.com>
2026-08-19 12:48:23 -0500

    chore: summary_tab.dart

--- lib/presentation/screens/inspection/summary_tab.dart
+++ lib/presentation/screens/inspection/summary_tab.dart
@@ -344,9 +344,6 @@ class _SummaryTabState extends ConsumerState<SummaryTab>
           .replaceFirst('car_', 'car-')
           .replaceFirst('int_', 'int-');
 
-      final allItems = InspectionCategory.buildAll().expand((c) => c.items).toList();
-      final itemLookup = {for (final item in allItems) item.id: item};
-
       final checklistResults = <String, Map<String, dynamic>>{};
       for (final entry in checkState.results.entries) {
         final r = entry.value;
@@ -362,8 +359,6 @@ class _SummaryTabState extends ConsumerState<SummaryTab>
             ItemStatus.defecto => 'DEFECTO',
             ItemStatus.noAplica => 'NO_APLICA',
           },
-          if (item != null) 'name': item.name,
-          if (item != null) 'subcategory': item.subcategory,
           if (r.comment.isNotEmpty) 'comment': r.comment,
           if (r.selectedChips.isNotEmpty) 'selectedChips': r.selectedChips,
         };
